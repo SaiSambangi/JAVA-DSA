745f58abd8466be5a30391910cd3594efd5bb1e8
Sambangi Sai <157018277+SaiSambangi@users.noreply.github.com>
2024-04-29 11:36:59 -0500

    Pattern14 java file is uploaded

---
+++ Java  Programs/Pattern14.java
@@ -0,0 +1,25 @@
+public class Pattern14 {
+    
+   
+        static void pattern14(int N)
+     {
+          
+           for(int i=0;i<N;i++){
+               
+               for(char ch = 'A'; ch<='A'+i;ch++){
+                   System.out.print(ch + " ");
+                   
+               }
+               System.out.println();
+              
+           }
+     }
+     
+         public static void main(String[] args) {
+             
+             int N = 5;
+             pattern14(N);
+         }
+     }
+     
+
