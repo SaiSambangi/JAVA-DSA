1293470b2c60d61f2a843c5ae7fde5336cb6630e
Sambangi Sai <157018277+SaiSambangi@users.noreply.github.com>
2024-05-01 11:38:03 -0500

    pattern16 java file is added

---
+++ Java  Programs/Pattern16.java
@@ -0,0 +1,27 @@
+public class Pattern16 {
+   
+        static void pattern16(int N)
+     {
+          
+           for(int i=0;i<N;i++){
+               
+               
+               for(int j=0;j<=i;j++){
+                   
+                   System.out.print((char)((int)('A'+i)) + " ");
+                   
+               }
+               System.out.println();
+              
+           }
+     }
+     
+         public static void main(String[] args) {
+             
+             int N = 5;
+             pattern16(N);
+         }
+     }
+     
+    
+
